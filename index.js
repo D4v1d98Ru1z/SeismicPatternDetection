79dc925a283984b9564f21b866a075357254c8c5
teamzz111 <aflr@outlook.com>
2018-10-01 02:14:24 -0500

    Sockets for communication

--- index.js
+++ index.js
@@ -4,20 +4,30 @@ const PORT = process.env.PORT || 5000
 
 var TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
 var fs = require('fs');
-
+var io = require('socket.io').listen(80); 
+// initiate socket.io server 
+io.sockets.on('connection', function (socket) {
+   socket.emit('news', { hello: 'world' });
+        socket.on('my other event', function (data) { 
+          console.log(data); 
+        }); 
+      }
+ ); 
 var textToSpeech = new TextToSpeechV1({
   username: '74f516e1-9e6e-474a-b69a-4b2db8cb9053',
   password: '3u2AX42XciPl',
   url: 'https://stream.watsonplatform.net/text-to-speech/api'
 });
 
-var params = {
-  text: 'Hola, Soy Watson (ya sé que tengo voz de mujer) y estoy a tu servicio, bienvenido al aplicativo desarollado por Andres y David está realizado con NodeJS. SaLudos especiales a Andres Largo',
-  voice: 'es-LA_SofiaVoice', // Optional voice
-  accept: 'audio/wav'
-};
 
-textToSpeech
+function hablar(data){
+  var params = {
+    text: data,
+    voice: 'es-LA_SofiaVoice', // Optional voice
+    accept: 'audio/wav'
+  };
+  
+  textToSpeech
   .synthesize(params, function(err, audio) {
     if (err) {
       console.log(err);
@@ -26,9 +36,11 @@ textToSpeech
     textToSpeech.repairWavHeader(audio);
     fs.writeFileSync('public/audio.wav', audio);
     console.log('audio.wav written with a corrected wav header');
+
+  });
+}
+  
     express()
       .use(express.static(path.join(__dirname, 'public')))
       .use(express.static(__dirname + '/public'))
       .listen(PORT, () => console.log(`Listening on ${PORT}`))
-});
-
